c40a8c81c61bb6b684806416ac9fd42f5264dc8c
adeee11 <you@example.comhello@iwebcode.design>
2021-12-02 17:23:15 +0530

    fixed

--- Src/Components/LocationCard/LocationCard.tsx
+++ Src/Components/LocationCard/LocationCard.tsx
@@ -1,10 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, Text, TouchableOpacity } from "react-native";
 import assets from "../../../assets";
 import { Location } from "../../../assets/svg";
 import { AntDesign } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from "./styles";
+import api from "../../globalStyles/api";
+import weatherApi from "../../api/axios";
+import WeatherImage from "../WeatherImage/WeatherImage";
+import Loader from "../Loader";
 
 const LocationCard = ({
   current,
@@ -14,9 +18,12 @@ const LocationCard = ({
   removeLocation,
   latitude,
   longitude,
-  showDetails
+  showDetails,
 }: any) => {
-  const [currentDetails, setCurrentDetails] = useState()
+  const [currentDetails, setCurrentDetails] = useState();
+  const [temperature, setTemperature] = useState<number>(0);
+  const [weatherMain, setWeatherMain] = useState();
+  const [loading, setLoading] = useState(false);
 
   const handleRemoveLocation = () => {
     const locationObject = {
@@ -24,7 +31,7 @@ const LocationCard = ({
       street: street,
       country: country,
       latitude: latitude,
-      longitude: longitude
+      longitude: longitude,
     };
     removeLocation(locationObject);
   };
@@ -35,39 +42,73 @@ const LocationCard = ({
       street: street,
       country: country,
       latitude: latitude,
-      longitude: longitude
+      longitude: longitude,
     };
-    showDetails(current ? currentDetails : locationObject)
-  }
+    showDetails(current ? currentDetails : locationObject);
+  };
 
   useEffect(() => {
-   (async () => {
-     try {
-       const data = await AsyncStorage.getItem("climeeCurrentLocation");
-       setCurrentDetails(JSON.parse(data))
-     } catch (err) {    
-       console.log(err);
-     }
-   })()
-  }, [current])
+    (async () => {
+      try {
+        const data: any = await AsyncStorage.getItem("climeeCurrentLocation");
+        setCurrentDetails(JSON.parse(data));
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [current]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const response: any = await weatherApi.get(
+          `/weather?lat=${current ? currentDetails?.latitude : latitude}&lon=${
+            current ? currentDetails?.longitude : longitude
+          }&appid=${api}&units=metric`
+        );
+        setTemperature(response?.data?.main?.temp);
+        setWeatherMain(response?.data?.weather[0]?.main);
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+      }
+    })();
+  }, [currentDetails?.latitude]);
 
   return (
     <>
-      <TouchableOpacity style={styles.currentLocationContainer} onPress={handleDetails}>
-        <Image
-          source={assets.sunnyRainy}
-          style={{
-            width: 40,
-            height: 40,
-            resizeMode: "contain",
-            marginHorizontal: 10,
-          }} />
-        <Text
-          style={[styles.tempText, { color: !current ? "#9B9B9B" : "#363B64" }]}>
-          24&deg;
-        </Text>
-        <View
-          style={{ alignItems: "flex-start", width: current ? "100%" : "61%" }}>
+      <TouchableOpacity
+        style={styles.currentLocationContainer}
+        onPress={handleDetails}
+      >
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <View
+              style={{
+                flex: 0.2,
+                flexDirection: "row",
+                paddingRight: 40,
+                alignItems: "center",
+              }}
+            >
+              <View style={{ marginHorizontal: 10 }}>
+                <WeatherImage img={weatherMain} height={40} width={40} />
+              </View>
+              <Text
+                style={[
+                  styles.tempText,
+                  { color: !current ? "#9B9B9B" : "#363B64" },
+                ]}
+              >
+                {temperature?.toFixed(0)}&deg;
+              </Text>
+            </View>
+          </>
+        )}
+        <View style={{ alignItems: "flex-start", flex: 0.7 }}>
           {current ? (
             <>
               <View style={{ flexDirection: "row", alignItems: "center" }}>
@@ -80,16 +121,20 @@ const LocationCard = ({
             </>
           ) : (
             <>
-              <Text style={styles.currentLocationText}>{street}</Text>
+              {street === null ? null : (
+                <Text style={styles.currentLocationText}>{street}</Text>
+              )}
               <Text style={styles.locationText}>
                 {city}, {country}
               </Text>
             </>
           )}
-          {/* <Text style={styles.currentLocationText}>{street}</Text> */}
         </View>
         {current ? null : (
-          <TouchableOpacity onPress={handleRemoveLocation}>
+          <TouchableOpacity
+            onPress={handleRemoveLocation}
+            style={{ flex: 0.1 }}
+          >
             <AntDesign name="star" size={24} color="#F5CA04" />
           </TouchableOpacity>
         )}
