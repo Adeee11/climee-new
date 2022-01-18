3d0ab54318ace603cfcebbf23501330ca2f3f54e
adeee11 <you@example.comhello@iwebcode.design>
2022-01-18 17:10:48 +0530

    notifications fixed

--- Src/api/strapi.ts
+++ Src/api/strapi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: "https://climee-cms-am6tk.ondigitalocean.app"
-  baseURL: "https://ea3a-2401-4900-1f32-4d1-85ab-4d10-1786-2526.ngrok.io"
+  baseURL: "https://climee-cms-am6tk.ondigitalocean.app"
+  // baseURL: "https://ea3a-2401-4900-1f32-4d1-85ab-4d10-1786-2526.ngrok.io"
 });
 
 export default instance; 
--- metro.config.js
+++ metro.config.js
@@ -3,19 +3,39 @@
 
 // module.exports = getDefaultConfig(__dirname);
 
-const { getDefaultConfig } = require("metro-config");
+// const { getDefaultConfig } = require("metro-config");
+
+// module.exports = (async () => {
+//   const {
+//     resolver: { sourceExts, assetExts }
+//   } = await getDefaultConfig();
+//   return {
+//     transformer: {
+//       babelTransformerPath: require.resolve("react-native-svg-transformer")
+//     },
+//     resolver: {
+//       assetExts: assetExts.filter(ext => ext !== "svg"),
+//       sourceExts: [...sourceExts, "svg"]
+//     }
+//   };
+// })();
+
+const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
+  const config = await getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
 })();
